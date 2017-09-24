21230607725e3ce5c4e325d5f5735603764958de
Winter LaMon <winter.lamon@gmail.com>
2017-09-24 20:32:23 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,9 @@
-
 def levitation_quiz
-	#your code here
+  loop do
+    puts "What is the spell that enacts levitation?"
+    spell = gets.chomp
+    break if spell == "Wingardium Leviosa"
+  end
 end
 
 
